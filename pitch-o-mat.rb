f12b9a8748c539a0bd968831acc605db3726380f
Colin <vogon@icculus.org>
2013-04-15 19:59:01 -0700

    pitch-o-mat 1.0

--- pitch-o-mat.rb
+++ pitch-o-mat.rb
@@ -1,8 +1,39 @@
 require 'sinatra'
 require 'slim'
+require 'yaml'
+
+require './gb-concept'
+require './gb-genre'
+
+CONCEPTS_PATH = "./concepts.yml"
+GENRES_PATH = "./genres.yml"
+
+class PitchomatHelpers
+	def initialize
+		puts "boop"
+		@concepts = YAML.load(File.open(CONCEPTS_PATH))
+		@genres = YAML.load(File.open(GENRES_PATH))
+	end
+
+	def random_genre
+		i = rand(@genres.length)
+		genre = @genres[i]
+
+		genre
+	end
+
+	def random_concept
+		i = rand(@concepts.length)
+		concept = @concepts[i]
+
+		concept
+	end	
+end
+
+PITCHOMAT = PitchomatHelpers.new
 
 class Pitchomat < Sinatra::Base
 	get '/pitchme' do
-		slim :pitch
+		slim :pitch, :locals => { :pitchomat => PITCHOMAT }
 	end
 end
