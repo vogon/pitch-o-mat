247ee450c4275482342190708fb7c0931b640907
Colin Bayer <vogon@icculus.org>
2013-04-16 00:35:11 -0700

    filter out pokemon from concepts list

--- pitch-o-mat.rb
+++ pitch-o-mat.rb
@@ -7,12 +7,25 @@ require './gb-genre'
 
 CONCEPTS_PATH = "./concepts.yml"
 GENRES_PATH = "./genres.yml"
+POKEMON_PATH = "./pokemon.txt"
 
 class PitchomatHelpers
 	def initialize
 		puts "boop"
 		@concepts = YAML.load(File.open(CONCEPTS_PATH))
 		@genres = YAML.load(File.open(GENRES_PATH))
+		@pokemon = load_pokemon
+	end
+
+	# load newline-delimited list of pokemon from file, strip off whitespace
+	def load_pokemon
+		File.open(POKEMON_PATH).readlines.map { |pkmn| pkmn.strip }
+	end
+
+	# some well-meaning asshole submitted every pokemon as a concept page to the GB wiki
+	# so we need to preflight concepts to make sure they're not pokemon
+	def is_pokemon?(concept)
+		!!(@pokemon.index(concept.name))
 	end
 
 	def random_genre
@@ -23,11 +36,14 @@ class PitchomatHelpers
 	end
 
 	def random_concept
-		i = rand(@concepts.length)
-		concept = @concepts[i]
+		# choose only non-pokemon concepts for now
+		begin
+			i = rand(@concepts.length)
+			concept = @concepts[i]
+		end while is_pokemon?(concept)
 
 		concept
-	end	
+	end
 end
 
 PITCHOMAT = PitchomatHelpers.new
