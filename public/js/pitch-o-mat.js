3bfbfe2e927724b84b3151d3402f133fb124a00c
Colin <vogon@icculus.org>
2013-04-28 14:17:03 -0700

    web fuckery

---
+++ public/js/pitch-o-mat.js
@@ -0,0 +1,12 @@
+"use strict";
+
+function addGenre(beforeIndex)
+{
+	alert("addGenre(" + beforeIndex + ")");
+}
+
+function addConcept(beforeIndex)
+{
+	alert("addConcept(" + beforeIndex + ")");
+}
+
